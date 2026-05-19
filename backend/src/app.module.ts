d9ce21c0e2365bb30db7277d046eda30e64a895f
Maxion38 <147092829+Maxion38@users.noreply.github.com>
2026-05-19 19:12:29 +0200

    added users feature

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { TrainingCoursesModule } from './training-courses/training-courses.modul
 import { ModulesModule } from './modules/modules.module';
 import { AuthModule } from './auth/auth.module';
 import { InvitationModule } from './invitation/invitation.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, TrainingCoursesModule, ModulesModule, AuthModule, InvitationModule],
+  imports: [PrismaModule, TrainingCoursesModule, ModulesModule, AuthModule, InvitationModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
